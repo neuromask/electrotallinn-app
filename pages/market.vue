4ca91e75d3dd8fa2c1f87da443e342e6fe7e28fe
Aleksandr <neuromask@gmail.com>
2021-11-18 19:32:46 +0200

    nav style

--- pages/market.vue
+++ pages/market.vue
@@ -5,6 +5,8 @@
                 <b-nav-item to="/" nuxt disabled><b-icon variant="dark" icon="arrow-return-right"></b-icon></b-nav-item>
                 <b-nav-item to="/market" nuxt exact>Products</b-nav-item>
                 <b-nav-item v-b-modal.product-modal>Add</b-nav-item>
+            </b-navbar-nav>
+            <b-navbar-nav class="ml-auto text-right">
                 <b-button variant="outline-secondary" v-if="$nuxt.$route.path == '/market'" size="sm" v-b-toggle.sidebar-right>Menu <b-icon icon="list" /></b-button>
             </b-navbar-nav>
         </b-navbar>
