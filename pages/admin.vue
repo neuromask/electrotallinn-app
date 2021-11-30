80fae70d996d3183fc0deefbdf49b0ab69f641ff
Aleksandr <neuromask@gmail.com>
2021-12-01 01:39:32 +0200

    edits

--- pages/admin.vue
+++ pages/admin.vue
@@ -1,6 +1,6 @@
 <template>
     <section>
-        <b-navbar type="dark" variant="light" class="mb-4 px-3 pt-2" sticky>
+        <b-navbar type="dark" variant="light" class="mb-4 px-3 pt-2">
             <b-navbar-nav>
                 <b-nav-item to="/" nuxt disabled><b-icon variant="dark" icon="arrow-return-right"></b-icon></b-nav-item>
                 <b-nav-item to="/admin" nuxt exact>Dashboard</b-nav-item>
