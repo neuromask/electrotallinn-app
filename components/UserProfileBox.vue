7c67c7f497b26f0b6c709e85157a3e3dcef72eae
Aleksandr <neuromask@gmail.com>
2021-12-17 00:08:44 +0200

    small edits

--- components/UserProfileBox.vue
+++ components/UserProfileBox.vue
@@ -5,7 +5,7 @@
                 <b-avatar :to="localePath(`/users/${$user.uin}`)" :src="$user.photoUrl" class="profile shadow-sm" size="5.5rem"></b-avatar>
                 <div class="ml-3 w-100">
                     <h5 class="mb-1"><strong class="text-dark">{{ $user.firstName }}</strong></h5>
-                    <nuxt-link :to="localePath(`/users/${$user.uin}`)"><b-button size="sm" variant="primary"><b-icon icon="person-bounding-box" /> {{ $t('nav.myProfile') }}</b-button></nuxt-link>
+                    <nuxt-link :to="localePath(`/users/${$user.uin}`)"><b-button size="sm" variant="primary" class="profile-btn"><b-icon icon="person-bounding-box" /> {{ $t('nav.myProfile') }}</b-button></nuxt-link>
                 </div>
             </div>
         </b-alert>
@@ -22,5 +22,8 @@
 }
 .bg-pat {
     background-image: url('~/assets/img/pattern-icons.png');
+}
+.profile-btn {
+    white-space: nowrap;
 }
 </style>
