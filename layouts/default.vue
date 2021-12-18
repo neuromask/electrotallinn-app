018ee2494ea22c4a7ea73cd93af9a1450ce93825
Aleksandr <neuromask@gmail.com>
2021-12-18 03:52:19 +0200

    def layout meta

--- layouts/default.vue
+++ layouts/default.vue
@@ -23,3 +23,35 @@
   min-height: 800px;  
 }
 </style>
+<script>
+export default {
+  
+  head() {
+    const i18nHead = this.$nuxtI18nHead({ addSeoAttributes: true })
+
+    return {
+      title: 'ElectroTallinn App',
+      htmlAttrs: {
+        lang: this.$i18n.locale,
+        ...i18nHead.htmlAttrs
+      },
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: this.$t('meta.description') },
+        { hid: "og:image", property: "og:image", content:`${this.$config.siteUrl}/app-et-${this.$i18n.locale}.jpg`, },
+        { hid: 'og:url', property: 'og:url', content: `https://app.electrotallinn.ee` },
+        { hid: "og:title", property: "og:title", content: 'ElectroTallinn App' },
+        { hid: "og:description", property: "og:description", content: this.$t('meta.description') },
+        { hid: "og:type", property: "og:type", content: 'website' },
+        ...i18nHead.meta
+      ],
+      link: [
+        { rel: 'icon', type: 'image/png', href: '/favicon.png' },
+        ...i18nHead.link
+      ]
+    }
+  }
+
+}
+</script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -3,32 +3,20 @@ export default {
   target: 'server',
 
   // Global page headers: https://go.nuxtjs.dev/config-head
-  head() {
-    const i18nHead = this.$nuxtI18nHead({ addSeoAttributes: true })
-    
-    return {
-      title: 'ElectroTallinn App',
-      htmlAttrs: {
-        lang: this.$i18n.locale,
-        ...i18nHead.htmlAttrs
-      },
-      meta: [
-        { charset: 'utf-8' },
-        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { hid: 'description', name: 'description', content: this.$t('meta.description') },
-        { hid: "og:image", property: "og:image", content:`${this.$config.siteUrl}/app-et-${this.$i18n.locale}.jpg`, },
-        { hid: 'og:url', property: 'og:url', content: `https://app.electrotallinn.ee` },
-        { hid: "og:title", property: "og:title", content: 'ElectroTallinn App' },
-        { hid: "og:description", property: "og:description", content: this.$t('meta.description') },
-        { hid: "og:type", property: "og:type", content: 'website' },
-        ...i18nHead.meta
-      ],
-      link: [
-        { rel: 'icon', type: 'image/png', href: '/favicon.png' },
-        ...i18nHead.link
-      ]
-    }
-
+  head: {
+    htmlAttrs: {
+      lang: "en-GB",
+    },
+    title: 'ElectroTallinn App',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Electric market, charging and repair map, ElectroPeople profiles.' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/png', href: '/favicon.png' },
+      { hid: "canonical", rel: "canonical", href: "https://app.electrotallinn.ee", },
+    ]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
