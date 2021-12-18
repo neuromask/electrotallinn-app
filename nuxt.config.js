f8c739405d55d65e7c787bfae01c64a277b68cf8
Aleksandr <neuromask@gmail.com>
2021-12-18 02:44:15 +0200

    revert head obj

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,34 +3,26 @@ export default {
   target: 'server',
 
   // Global page headers: https://go.nuxtjs.dev/config-head
-  head() {
-    const i18nHead = this.$nuxtI18nHead({ addSeoAttributes: true })
-    
-    return {
-      title: 'ElectroTallinn App',
-      htmlAttrs: {
-        lang: this.$i18n.locale,
-        ...i18nHead.htmlAttrs
-      },
-      meta: [
-        { charset: 'utf-8' },
-        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { hid: 'description', name: 'description', content: this.$t('meta.description') },
-        { hid: "og:image", property: "og:image", content:`${this.$config.siteUrl}/app-et-${this.$i18n.locale}.jpg`, },
-        { property: "og:image:width", content: "1920" },
-        { property: "og:image:height", content: "1080" },
-        { hid: 'og:url', property: 'og:url', content: `https://app.electrotallinn.ee` },
-        { hid: "og:title", property: "og:title", content: 'ElectroTallinn App' },
-        { hid: "og:description", property: "og:description", content: this.$t('meta.description') },
-        { hid: "og:type", property: "og:type", content: 'website' },
-        ...i18nHead.meta
-      ],
-      link: [
-        { rel: 'icon', type: 'image/png', href: '/favicon.png' },
-        ...i18nHead.link
-      ]
-    }
-
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    htmlAttrs: {
+      lang: this.$i18n.locale
+    },
+    title: 'ElectroTallinn App',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: this.$t('meta.description') },
+      { hid: "og:image", property: "og:image", content:`${this.$config.siteUrl}/app-et-${this.$i18n.locale}.jpg`, },
+      { hid: 'og:url', property: 'og:url', content: `https://app.electrotallinn.ee` },
+      { hid: "og:title", property: "og:title", content: 'ElectroTallinn App' },
+      { hid: "og:description", property: "og:description", content: this.$t('meta.description') },
+      { hid: "og:type", property: "og:type", content: 'website' },
+    ],
+    link: [
+      { rel: 'icon', type: 'image/png', href: '/favicon.png' },
+      { hid: "canonical", rel: "canonical", href: "https://app.electrotallinn.ee", },
+    ]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
@@ -73,7 +65,7 @@ export default {
     '@nuxtjs/axios',
     '@nuxtjs/i18n',
     // https://go.nuxtjs.dev/pwa
-    // '@nuxtjs/pwa',
+    '@nuxtjs/pwa',
     '@nuxtjs/toast'
   ],
 
