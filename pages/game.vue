383cc18b725e319d9fe4596963e131be0ec5f050
Aleksandr <neuromask@gmail.com>
2021-12-23 23:38:57 +0200

    game seo

--- pages/game.vue
+++ pages/game.vue
@@ -5,15 +5,12 @@
                 <b-nav-item :to="localePath('/')" nuxt disabled><b-icon variant="dark" icon="arrow-return-right"></b-icon></b-nav-item>
                 <b-nav-item :to="localePath('/game')" nuxt exact>{{ $t('nav.game') }}</b-nav-item>
             </b-navbar-nav>
-            <!--<b-navbar-nav class="ml-auto text-right">
-                <b-button variant="outline-secondary" v-if="$nuxt.$route.path == '/market'" size="sm" v-b-toggle.menuOptions>Menu <b-icon icon="list" /></b-button>
-            </b-navbar-nav>-->
         </b-navbar>
         <NuxtChild />
     </div>
 </template>
 <script>
 export default {
-  head() { return { title: this.$t("nav.market"), } }
+  head() { return { title: this.$t("nav.game"), } }
 }
 </script>
