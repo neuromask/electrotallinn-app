a189d01fb3305c6a3482a96c06ba7103cc22404b
Aleksandr <neuromask@gmail.com>
2021-12-24 21:14:10 +0200

    game clean code

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,7 +6,7 @@
       <Banner />
       <b-container class="bg-light mt-3 px-5 pt-0 main-container rounded position-relative">
         <NavBar />
-          <b-alert v-if="!this.$user.isLogged" class="mt-3 mb-0" variant="warning" show dismissible><b-icon icon="arrow-up" /> {{ $t('main.loginToastWarning') }}</b-alert>
+          <b-alert v-show="!this.$user.isLogged" class="mt-3 mb-0" variant="warning" show dismissible><b-icon icon="arrow-up" /> {{ $t('main.loginToastWarning') }}</b-alert>
         <Nuxt />
       </b-container>
     </main>
