7da5e1ca8cb9139fd7dc85a8ccd26849f41f9274
Aleksandr <neuromask@gmail.com>
2021-12-29 23:54:17 +0200

    site top img

--- components/Banner.vue
+++ components/Banner.vue
@@ -1,11 +1,11 @@
 <template>
-  <NuxtLink :to="localePath('/')"><b-img-lazy src="~assets/img/top.jpg" center id="banner" /></NuxtLink>
+  <NuxtLink :to="localePath('/')"><b-img-lazy src="~assets/img/site-top.svg" center id="banner" /></NuxtLink>
 </template>
 <style>
 #banner {
   width: 100%;
-  height:342px;
-  max-width: 2048px;
+  height: 30vw;
+  max-height:342px;
   object-fit: cover;
 }
 </style>
