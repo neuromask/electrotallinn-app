b454f3e3ff9bad48cedcd871143043f812b223fb
Aleksandr <neuromask@gmail.com>
2022-01-03 17:09:29 +0200

    l yt

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -125,10 +125,10 @@
         </div>
         <b-row>
           <b-col class="mb-3" cols="12" lg="6">
-            <LazyYoutube src="https://www.youtube.com/embed/M0ezw-mZuDQ" />
+            <LazyYoutube src="https://www.youtube.com/watch?v=M0ezw-mZuDQ" />
           </b-col>
           <b-col cols="12" lg="6">
-            <LazyYoutube src="https://www.youtube.com/embed/ovVijvjmlt4" />
+            <LazyYoutube src="https://www.youtube.com/watch?v=ovVijvjmlt4" />
           </b-col>
         </b-row>
       </div>
