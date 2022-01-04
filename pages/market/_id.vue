c65836877e63f12aeb661bd3739335acfa2cced3
Aleksandr <neuromask@gmail.com>
2022-01-05 01:08:56 +0200

    product style

--- pages/market/_id.vue
+++ pages/market/_id.vue
@@ -4,26 +4,26 @@
     <CoolLightBox :items="items" :index="index" :effect="'fade'" @close="index = null" />
     <b-card-group columns>
       <b-card class="shadow-sm">
-        <div class="d-flex justify-content-between align-items-start">
-          <h3 class="underline font-weight-bold">{{ $t('main.info') }}</h3>
-          <h2 class="m-0 text-nowrap text-warning" v-if="product.price"><b-badge variant="primary">{{ product.price }}€</b-badge></h2>
-        </div>
+        <h3 class="underline font-weight-bold">{{ $t('main.info') }}</h3>
         <h3 class="mb-3" variant="light" v-if="product.name">{{ product.name }}</h3>
-        <b-list-group class="text-left">
-          <b-list-group-item variant="light" v-if="product.description">
-            <h5 class="mb-1 font-weight-bold">{{ $t('main.description') }}</h5>
+        <b-list-group>
+          <b-list-group-item class="text-secondary" variant="light" v-if="product.description">
+            <div class="d-flex justify-content-between align-items-start">
+              <h4 class="font-weight-bold">{{ $t('main.description') }}</h4>
+              <h3 class="m-0 text-nowrap text-warning" v-if="product.price"><b-badge variant="primary">{{ product.price }}€</b-badge></h3>
+            </div>
             <p class="mb-0 pre" v-html="linkify(product.description)"></p>
           </b-list-group-item>
-          <b-list-group-item variant="light" v-if="product.price">{{ $t('main.price') }}: <strong>{{ product.price }} €</strong></b-list-group-item>
-          <b-list-group-item variant="light" v-if="product.category">{{ $t('main.category') }}: <strong>{{ $t('market.category.' + product.category) }}</strong></b-list-group-item>
-          <b-list-group-item variant="light" v-if="product.dateCreated">{{ $t('main.dateAdded') }}: <strong>{{ new Date(product.dateCreated).toLocaleDateString($i18n.locale, { year:"numeric", month:"short", day: 'numeric' }) }}</strong></b-list-group-item>
+          <b-list-group-item variant="light" v-if="product.price" class="text-secondary">{{ $t('main.price') }}: <strong>{{ product.price }} €</strong></b-list-group-item>
+          <b-list-group-item variant="light" v-if="product.category" class="text-secondary">{{ $t('main.category') }}: <strong>{{ $t('market.category.' + product.category) }}</strong></b-list-group-item>
+          <b-list-group-item variant="light" v-if="product.dateCreated" class="text-secondary">{{ $t('main.dateAdded') }}: <strong>{{ new Date(product.dateCreated).toLocaleDateString($i18n.locale, { year:"numeric", month:"short", day: 'numeric' }) }}</strong></b-list-group-item>
         </b-list-group>
       </b-card>
       <b-card class="shadow-sm">
        <h3 class="underline font-weight-bold">{{ $t('main.contact') }}</h3>
         <b-list-group class="text-left">
-          <b-list-group-item variant="light" v-if="product.userFirstName">{{ $t('main.addedBy') }}: <nuxt-link :to="`/users/${product.userUin}`"><strong>{{ product.userFirstName }}</strong></nuxt-link></b-list-group-item>
-          <b-list-group-item variant="light" v-if="product.username">{{ $t('profile.telegram') }}: <a :href="'https://t.me/'+product.username" target="_blank"><strong>{{ product.username }}</strong></a></b-list-group-item>
+          <b-list-group-item variant="light" class="text-secondary" v-if="product.userFirstName">{{ $t('main.addedBy') }}: <nuxt-link :to="`/users/${product.userUin}`"><strong>{{ product.userFirstName }}</strong></nuxt-link></b-list-group-item>
+          <b-list-group-item variant="light" class="text-secondary" v-if="product.username">{{ $t('profile.telegram') }}: <a :href="'https://t.me/'+product.username" target="_blank"><strong>{{ product.username }}</strong></a></b-list-group-item>
         </b-list-group>
         <b-alert v-if="!product.username" class="mt-3 mb-0" show variant="warning">
           <i18n path="main.sellerNoUsername" tag="p" class="mb-0">
