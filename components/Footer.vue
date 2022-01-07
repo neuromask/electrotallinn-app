0494c79cfe9296cabe3a5495e52853ef9de1d2d6
Aleksandr <neuromask@gmail.com>
2022-01-07 16:01:00 +0200

    footer upd

--- components/Footer.vue
+++ components/Footer.vue
@@ -5,18 +5,18 @@
       <b-row>
         <b-col cols="12" lg="4" class="my-3 d-flex justify-content-center align-items-center">
           <div>
-            <p><b-img-lazy block left class="w-25 mr-2" fluid src="~/assets/img/circle.svg" />{{ $t('main.goalText') }}</p>
+            <p class="small"><b-img-lazy block left class="w-25 mr-2" fluid src="~/assets/img/circle.svg" />{{ $t('main.goalText') }}</p>
             <BotButton />
           </div>
         </b-col>
         <b-col cols="12" lg="4" class="my-3 d-flex justify-content-center align-items-center">
           <div>
-            <div class="mb-2"><Copyright /></div>
+            <div class="mb-3"><Menu /></div>
             <SocialLinks />
           </div>
         </b-col>
-        <b-col cols="12" lg="4" class="my-3">
-          <div class="ml-auto w-75"><Menu /></div>
+        <b-col cols="12" lg="4" class="my-3 d-flex justify-content-center align-items-center">
+          <Copyright />
         </b-col>
       </b-row>
     </b-container>
